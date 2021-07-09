078d8fbe5975578f5203484e09ec8c45799a2f14
jonathan <jonaf2103@gmail.com>
2021-07-09 16:44:12 -0300

    Adding dist to make this library usable through git from the package.json, this is temporary, we need to publish this into npm to remove the dist folder and add it to gitignore

---
+++ dist/index.d.ts
@@ -0,0 +1,27 @@
+declare const _default: {
+    EnvelopingTypes: any;
+    ICustomSmartWalletFactory: any;
+    IDeployVerifier: any;
+    IForwarder: any;
+    IPenalizer: any;
+    IRelayHub: any;
+    IRelayVerifier: any;
+    ISmartWalletFactory: any;
+    ITokenHandler: any;
+    IVersionRegistry: any;
+    IWalletCustomLogic: any;
+    IWalletFactory: any;
+    CustomSmartWallet: any;
+    CustomSmartWalletDeployVerifier: any;
+    CustomSmartWalletFactory: any;
+    DeployVerifier: any;
+    Penalizer: any;
+    RelayHub: any;
+    RelayVerifier: any;
+    SmartWallet: any;
+    SmartWalletFactory: any;
+    TestDeployVerifierEverythingAccepted: any;
+    TestVerifierEverythingAccepted: any;
+    VersionRegistry: any;
+};
+export default _default;
