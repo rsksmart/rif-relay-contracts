41c5331e5130eba96cd8750875e85b59fd2ce157
mortelli <ortellimarcelo@gmail.com>
2021-10-27 18:14:51 -0300

    update dist

--- types/truffle-contracts/index.d.ts
+++ types/truffle-contracts/index.d.ts
@@ -2625,7 +2625,7 @@ export interface IRelayHubInstance extends Truffle.ContractInstance {
     txDetails?: Truffle.TransactionDetails
   ): Promise<boolean>;
 
-  getRelayData(
+  getRelayInfo(
     relayManager: string | BN,
     txDetails?: Truffle.TransactionDetails
   ): Promise<{
@@ -4388,7 +4388,7 @@ export interface RelayHubInstance extends Truffle.ContractInstance {
     ): Promise<number>;
   };
 
-  getRelayData(
+  getRelayInfo(
     relayManager: string | BN,
     txDetails?: Truffle.TransactionDetails
   ): Promise<{
