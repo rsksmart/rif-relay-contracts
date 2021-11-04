28f95b96d9080d94d7f87725dfe6a6f375f6f1ff
mortelli <ortellimarcelo@gmail.com>
2021-11-04 11:29:12 -0300

    update dist

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,4 +1,4 @@
-import { RelayManagerData } from "./types/RelayManagerData";
+import { RelayManagerData } from './types/RelayManagerData';
 declare const EnvelopingTypes: any;
 declare const ICustomSmartWalletFactory: any;
 declare const IDeployVerifier: any;
