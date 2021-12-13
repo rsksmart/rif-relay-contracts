454b2fb700d931a1ff273b52e276dde0a35eea27
mortelli <ortellimarcelo@gmail.com>
2021-12-12 23:45:50 -0300

    iterate Collector and ICollector contracts

--- contracts/Collector.sol
+++ contracts/Collector.sol
@@ -1,6 +1,21 @@
 // SPDX-License-Identifier:MIT
 pragma solidity ^0.6.12;
+pragma experimental ABIEncoderV2;
 
-contract Collector{
+import "./interfaces/ICollector.sol";
 
+contract Collector is ICollector{
+
+    Shares revenueShares;
+
+    constructor(
+        Shares memory _shares
+    )public {
+        require(
+            _shares.relayOperator.beneficiary &&
+            _shares.walletProvider.beneficiary &&
+            _shares.liquidityProvider.beneficiary && 
+            _shares.iovLabsRecipient.beneficiary
+        );
+    }
 }
