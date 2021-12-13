d833dedc54ac7fa7c6eb24a22ac2bb440bc8b724
mortelli <ortellimarcelo@gmail.com>
2021-12-13 15:47:52 -0300

    iterate Collector contract

--- contracts/Collector.sol
+++ contracts/Collector.sol
@@ -6,11 +6,23 @@ import "./interfaces/ICollector.sol";
 
 contract Collector is ICollector{
 
-    Shares revenueShares;
+    Shares private revenueShares;
 
-    constructor(
-        Shares memory _shares
-    )public {
+    constructor(Shares memory _shares)
+    public
+    validShares(_shares)
+    {
+        revenueShares = _shares;
+    }
+
+    function updateShares(Shares memory _shares) 
+    public
+    validShares(_shares)
+    {
+        revenueShares = _shares;
+    }
+
+    modifier validShares(Shares memory _shares){
         require(_shares.relayOperator.beneficiary != address(0), "relayOperator must be set");
         require(_shares.walletProvider.beneficiary != address(0), "walletProvider must be set");
         require(_shares.liquidityProvider.beneficiary != address(0), "liquidityProvider must be set");
@@ -23,5 +35,7 @@ contract Collector is ICollector{
             _shares.iovLabsRecipient.share == 100,
             "total shares must add up to 100%"
         );
+
+        _;
     }
 }
