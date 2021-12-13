caa3b48a2bdd669ff64a0434d377258885d12aca
mortelli <ortellimarcelo@gmail.com>
2021-12-13 17:19:00 -0300

    add withdraw and receive functions for collector funds

--- contracts/Collector.sol
+++ contracts/Collector.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.6.12;
 pragma experimental ABIEncoderV2;
 
 import "./interfaces/ICollector.sol";
+import "@openzeppelin/contracts/math/SafeMath.sol";
 
 contract Collector is ICollector{
 
@@ -37,5 +38,17 @@ contract Collector is ICollector{
         );
 
         _;
+    }
+
+    // solhint-disable-next-line no-empty-blocks
+    receive() external payable{}
+
+    function withdraw() public {
+        uint balance = address(this).balance;
+        
+        revenueShares.relayOperator.beneficiary.transfer(SafeMath.mul(SafeMath.div(balance, 100), revenueShares.relayOperator.share));
+        revenueShares.walletProvider.beneficiary.transfer(SafeMath.mul(SafeMath.div(balance, 100), revenueShares.walletProvider.share));
+        revenueShares.liquidityProvider.beneficiary.transfer(SafeMath.mul(SafeMath.div(balance, 100), revenueShares.liquidityProvider.share));
+        revenueShares.iovLabsRecipient.beneficiary.transfer(SafeMath.mul(SafeMath.div(balance, 100), revenueShares.iovLabsRecipient.share));
     }
 }
