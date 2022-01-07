cb1c9b7130a71942f34a967bc43424e91b3e09ab
mortelli <ortellimarcelo@gmail.com>
2022-01-07 10:54:50 -0300

    revenue sharing POC (#14)
    * Update the contract-addresses.json file to include the Testnet addresses. (#13)
    
    * Add some utility scripts can be used for creating a safe, testing it and executing the withdraw function
    
    * Print collector balance during the withdraw function
    
    * Move safeFactoryABI to a specific file and use the revenue-sharing-addresses file generated
    
    * Apply PR suggestions
    
    Co-authored-by: Antonio Morrone <antonio@iovlabs.org>

---
+++ tasks/create-safe.js
@@ -0,0 +1,130 @@
+const { contractNetworks } = require('./utils');
+const SafeFactory = require('./safeFactory.json');
+
+const setupFunctionDefinition = {
+    constant: false,
+    inputs: [
+        {
+            internalType: 'address[]',
+            name: '_owners',
+            type: 'address[]'
+        },
+        {
+            internalType: 'uint256',
+            name: '_threshold',
+            type: 'uint256'
+        },
+        {
+            internalType: 'address',
+            name: 'to',
+            type: 'address'
+        },
+        {
+            internalType: 'bytes',
+            name: 'data',
+            type: 'bytes'
+        },
+        {
+            internalType: 'address',
+            name: 'fallbackHandler',
+            type: 'address'
+        },
+        {
+            internalType: 'address',
+            name: 'paymentToken',
+            type: 'address'
+        },
+        {
+            internalType: 'uint256',
+            name: 'payment',
+            type: 'uint256'
+        },
+        {
+            internalType: 'address payable',
+            name: 'paymentReceiver',
+            type: 'address'
+        }
+    ],
+    name: 'setup',
+    outputs: [],
+    payable: false,
+    stateMutability: 'nonpayable',
+    type: 'function'
+};
+const ZERO_ADDRESS = `0x${'0'.repeat(40)}`;
+const EMPTY_DATA = '0x';
+const RevenueSharingAddresses = require('../revenue-sharing-addresses.json');
+
+async function deploySafe(web3, safeMasterCopyAddress, safeProxyFactoryAddress, owners, threshold, initialBalance) {
+    const accounts = await web3.eth.getAccounts();
+    const sender = accounts[0];
+
+    const safeFactoryContract = new web3.eth.Contract(
+        SafeFactory.abi,
+        safeProxyFactoryAddress
+    );
+    const encodedSetupFunction = web3.eth.abi.encodeFunctionCall(
+        setupFunctionDefinition,
+        [
+            owners,
+            threshold,
+            ZERO_ADDRESS,
+            EMPTY_DATA,
+            ZERO_ADDRESS,
+            ZERO_ADDRESS,
+            0,
+            ZERO_ADDRESS
+        ]
+    );
+    const createTx = await safeFactoryContract.methods
+        .createProxy(safeMasterCopyAddress, encodedSetupFunction)
+        .send({ from: sender, gas: '1081903' });
+    
+    const safeAddressCreated =
+        createTx?.events?.ProxyCreation?.returnValues.proxy;
+    console.log({ safeAddressCreated });
+
+    // give some rbtc to the safe account just created
+    await web3.eth.sendTransaction({
+        to: safeAddressCreated,
+        value: initialBalance,
+        from: sender
+    });
+    return safeAddressCreated;
+}
+
+module.exports = async (callback) => {
+    const chainId = await web3.eth.getChainId();
+    const {
+        relayOperator,
+        walletProvider,
+        liquidityProvider,
+        iovLabsRecipient
+    } = RevenueSharingAddresses[chainId.toString()];
+
+    const { safeMasterCopyAddress, safeProxyFactoryAddress } = contractNetworks[chainId];
+
+    const owners = [
+        relayOperator,
+        walletProvider,
+        liquidityProvider,
+        iovLabsRecipient
+    ];
+    const threshold = owners.length;
+    const initialBalance = web3.utils.toWei('10');
+
+    const safeAddress = await deploySafe(
+        web3,
+        safeMasterCopyAddress,
+        safeProxyFactoryAddress,
+        owners,
+        threshold,
+        initialBalance
+    );
+
+    const safeBalance = await web3.eth.getBalance(safeAddress);
+    console.log('Safe balance', web3.utils.fromWei(safeBalance));
+
+    // try to execute a transaction, but then we need to move this part into anther task probably
+    callback();
+};
