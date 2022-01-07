48cf9a49cf70dac8fac8d93950c03082ba0da2b4
mortelli <ortellimarcelo@gmail.com>
2022-01-07 10:54:50 -0300

    revenue sharing POC (#14)
    * Update the contract-addresses.json file to include the Testnet addresses. (#13)
    
    * Add some utility scripts can be used for creating a safe, testing it and executing the withdraw function
    
    * Print collector balance during the withdraw function
    
    * Move safeFactoryABI to a specific file and use the revenue-sharing-addresses file generated
    
    * Apply PR suggestions
    
    Co-authored-by: Antonio Morrone <antonio@iovlabs.org>

---
+++ tasks/withdraw.js
@@ -0,0 +1,103 @@
+const safeCoreSdk = require('@gnosis.pm/safe-core-sdk');
+const yargs = require('yargs/yargs');
+const { hideBin } = require('yargs/helpers');
+const {
+    getTestTokenInstance,
+    getCollectorInstance,
+    signWithAddress,
+    contractNetworks
+} = require('./utils');
+const argv = yargs(hideBin(process.argv)).parserConfiguration({
+    'parse-numbers': false
+}).argv;
+const RevenueSharingAddresses = require('../revenue-sharing-addresses.json');
+const { default: Safe, Web3Adapter } = safeCoreSdk;
+
+const printStatus = async (collectorAddress, owners, testTokenInstance) => {
+    const collectorBalance = await testTokenInstance.balanceOf(collectorAddress);
+    console.log(`Collector balance: ${collectorBalance}`);
+    for (const owner of owners) {
+        const balance = await testTokenInstance.balanceOf(owner);
+        console.log(`Address ${owner} balance: ${balance}`);
+    }
+};
+
+module.exports = async (callback) => {
+    const chainId = await web3.eth.getChainId();
+    const {
+        relayOperator,
+        walletProvider,
+        liquidityProvider,
+        iovLabsRecipient
+    } = RevenueSharingAddresses[chainId.toString()];
+
+    const owners = [
+        relayOperator,
+        walletProvider,
+        liquidityProvider,
+        iovLabsRecipient
+    ];
+
+    const { safeAddress } = argv;
+    if (!web3.utils.isAddress(safeAddress)) {
+        callback(new Error(`invalid "safeAddress": ${safeAddress}`));
+    }
+    const collectorInstance = await getCollectorInstance(artifacts);
+    const collectorAddress = collectorInstance.address;
+
+    // print the initial status
+    const safeBalance = await web3.eth.getBalance(safeAddress);
+    console.log('Safe balance', web3.utils.fromWei(safeBalance));
+    const testTokenInstance = await getTestTokenInstance(artifacts);
+
+    console.log('---Token balance before---');
+    await printStatus(collectorAddress, owners, testTokenInstance);
+
+    const ethAdapterOwner1 = new Web3Adapter({
+        web3,
+        signerAddress: owners[0]
+    });
+    const safeSdk = await Safe.create({
+        ethAdapter: ethAdapterOwner1,
+        safeAddress,
+        contractNetworks
+    });
+
+    const encodedWithdrawFunction = web3.eth.abi.encodeFunctionCall(
+        {
+            name: 'withdraw',
+            type: 'function',
+            inputs: []
+        },
+        []
+    );
+
+    const transactions = [
+        {
+            to: collectorAddress,
+            value: 0,
+            data: encodedWithdrawFunction
+        }
+    ];
+    const safeTransaction = await safeSdk.createTransaction(...transactions);
+
+    // All the owners but the first one will sign the transaction before,
+    // while the owners[0] will sign the transaction on execution.
+    for (let index = 1; index < owners.length; index++) {
+        const owner = owners[index];
+        await signWithAddress(web3, safeSdk, safeTransaction, owner);
+    }
+
+    // owner1 execute (and implicitly signs) the transaction
+    const safeTxResponse = await safeSdk.executeTransaction(safeTransaction, {
+        // we need to manually set the gasLimit in order to execute multisig tx on rsk
+        gasLimit: '1081903'
+    });
+    await safeTxResponse.transactionResponse?.wait();
+
+    console.log('---Token balance after---');
+    await printStatus(collectorAddress, owners, testTokenInstance);
+
+    // try to execute a transaction, but then we need to move this part into anther task probably
+    callback();
+};
