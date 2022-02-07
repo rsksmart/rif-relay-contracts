efb2a59f79d8c252b8c060318ddb766bbde29a84
Antonio Morrone <antonio@iovlabs.org>
2022-02-07 20:00:23 +0000

    PP-122: Fix the withdraw script to reflect the changes applied to 'revenue-sharing-addresses.json' (#19)
    * PP-122: Fix the withdraw script
    
    * Run 'prettier:fix'

--- tasks/create-safe.js
+++ tasks/create-safe.js
@@ -54,7 +54,14 @@ const setupFunctionDefinition = {
 const ZERO_ADDRESS = `0x${'0'.repeat(40)}`;
 const EMPTY_DATA = '0x';
 
-async function deploySafe(web3, safeMasterCopyAddress, safeProxyFactoryAddress, owners, threshold, initialBalance) {
+async function deploySafe(
+    web3,
+    safeMasterCopyAddress,
+    safeProxyFactoryAddress,
+    owners,
+    threshold,
+    initialBalance
+) {
     const accounts = await web3.eth.getAccounts();
     const sender = accounts[0];
 
@@ -78,7 +85,7 @@ async function deploySafe(web3, safeMasterCopyAddress, safeProxyFactoryAddress, 
     const createTx = await safeFactoryContract.methods
         .createProxy(safeMasterCopyAddress, encodedSetupFunction)
         .send({ from: sender, gas: '1081903' });
-    
+
     const safeAddressCreated =
         createTx?.events?.ProxyCreation?.returnValues.proxy;
     console.log({ safeAddressCreated });
@@ -95,12 +102,13 @@ async function deploySafe(web3, safeMasterCopyAddress, safeProxyFactoryAddress, 
 module.exports = async (callback) => {
     const chainId = await web3.eth.getChainId();
     // These addresses are the same of the ones used in migrations/3_revenue_sharing.js
-    const relayOperator = '0x7986b3DF570230288501EEa3D890bd66948C9B79';     // accounts[1]
-    const walletProvider = '0x0a3aA774752ec2042c46548456c094A76C7F3a79';    // accounts[2] 
-    const liquidityProvider = '0xCF7CDBbB5F7BA79d3ffe74A0bBA13FC0295F6036'; // accounts[3] 
-    const iovLabsRecipient = '0x39B12C05E8503356E3a7DF0B7B33efA4c054C409';  // accounts[4] 
+    const relayOperator = '0x7986b3DF570230288501EEa3D890bd66948C9B79'; // accounts[1]
+    const walletProvider = '0x0a3aA774752ec2042c46548456c094A76C7F3a79'; // accounts[2]
+    const liquidityProvider = '0xCF7CDBbB5F7BA79d3ffe74A0bBA13FC0295F6036'; // accounts[3]
+    const iovLabsRecipient = '0x39B12C05E8503356E3a7DF0B7B33efA4c054C409'; // accounts[4]
 
-    const { safeMasterCopyAddress, safeProxyFactoryAddress } = contractNetworks[chainId];
+    const { safeMasterCopyAddress, safeProxyFactoryAddress } =
+        contractNetworks[chainId];
 
     const owners = [
         relayOperator,
