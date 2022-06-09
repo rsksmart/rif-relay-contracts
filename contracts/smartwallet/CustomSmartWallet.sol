d580af1130150237e8b61972770aa7f7ab730c17
Andres Quijano <andres.quijano@iovlabs.org>
2022-06-09 12:15:41 -0500

    Changes CustomSmartWallet contract in order to avoid using the domainSeparator as parameter and also as part of the request

--- contracts/smartwallet/CustomSmartWallet.sol
+++ contracts/smartwallet/CustomSmartWallet.sol
@@ -14,15 +14,27 @@ contract CustomSmartWallet is IForwarder {
 
     uint256 public override nonce;
     bytes32 public constant DATA_VERSION_HASH = keccak256("2");
+    bytes32 public domainSeparator;    
+    
+    function buildDomainSeparator() internal {
+        domainSeparator = keccak256(
+            abi.encode(
+                keccak256("EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)"), //hex"8b73c3c69bb8fe3d512ecc4cf759cc79239f7b179b0ffacaa9a75d522b39400f",
+                keccak256("RSK Enveloping Transaction"), //DOMAIN_NAME hex"d41b7f69f4d7734774d21b5548d74704ad02f9f1545db63927a1d58479c576c8"
+                DATA_VERSION_HASH,
+                getChainID(),
+                address(this)
+            )
+        );
+    }  
 
     function verify(
-        bytes32 domainSeparator,
+        // bytes32 domainSeparator,
         bytes32 suffixData,
         ForwardRequest memory req,
         bytes calldata sig
     ) external override view {
-
-        _verifySig(domainSeparator, suffixData, req, sig);
+        _verifySig(/*domainSeparator, */suffixData, req, sig);
     }
 
     function getOwner() private view returns (bytes32 owner){
@@ -101,7 +113,7 @@ contract CustomSmartWallet is IForwarder {
     }
     
     function execute(
-        bytes32 domainSeparator,
+        // bytes32 domainSeparator,
         bytes32 suffixData,
         ForwardRequest memory req,
         bytes calldata sig
@@ -118,7 +130,7 @@ contract CustomSmartWallet is IForwarder {
         (sig);
         require(msg.sender == req.relayHub, "Invalid caller");
 
-        _verifySig(domainSeparator, suffixData, req, sig);
+        _verifySig(/*domainSeparator, */suffixData, req, sig);
         nonce++;
 
         if(req.tokenAmount > 0){
@@ -179,7 +191,7 @@ contract CustomSmartWallet is IForwarder {
     }
 
     function _verifySig(
-        bytes32 domainSeparator,
+        // bytes32 domainSeparator,
         bytes32 suffixData,
         ForwardRequest memory req,
         bytes memory sig
@@ -194,15 +206,15 @@ contract CustomSmartWallet is IForwarder {
         //Verify nonce
         require(nonce == req.nonce, "nonce mismatch");
 
-        require(
-            keccak256(abi.encode(
-                keccak256("EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)"),
-                keccak256("RSK Enveloping Transaction"), //DOMAIN_NAME
-                DATA_VERSION_HASH,
-                getChainID(),
-                address(this))) == domainSeparator,
-            "Invalid domain separator"
-        );
+        // require(
+        //     keccak256(abi.encode(
+        //         keccak256("EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)"),
+        //         keccak256("RSK Enveloping Transaction"), //DOMAIN_NAME
+        //         DATA_VERSION_HASH,
+        //         getChainID(),
+        //         address(this))) == domainSeparator,
+        //     "Invalid domain separator"
+        // );
         
         require(
             RSKAddrValidator.safeEquals(
@@ -210,7 +222,7 @@ contract CustomSmartWallet is IForwarder {
                     "\x19\x01",
                     domainSeparator,
                     keccak256(_getEncoded(suffixData, req)))
-                ).recover(sig), req.from), "signature mismatch"
+                ).recover(sig), req.from), "Signature mismatch"
         );
     }
 
@@ -220,7 +232,7 @@ contract CustomSmartWallet is IForwarder {
     ) private pure returns (bytes memory) {
         return
             abi.encodePacked(
-                keccak256("RelayRequest(address relayHub,address from,address to,address tokenContract,uint256 value,uint256 gas,uint256 nonce,uint256 tokenAmount,uint256 tokenGas,bytes data,RelayData relayData)RelayData(uint256 gasPrice,bytes32 domainSeparator,address relayWorker,address callForwarder,address callVerifier)"), //requestTypeHash,
+                keccak256("RelayRequest(address relayHub,address from,address to,address tokenContract,uint256 value,uint256 gas,uint256 nonce,uint256 tokenAmount,uint256 tokenGas,bytes data,RelayData relayData)RelayData(uint256 gasPrice,address relayWorker,address callForwarder,address callVerifier)"), //requestTypeHash,
                 abi.encode(
                     req.relayHub,
                     req.from,
@@ -322,6 +334,8 @@ contract CustomSmartWallet is IForwarder {
                 )
             }
         }
+
+        buildDomainSeparator();
     }
 
     function _fallback() private {
