d9bd59bf4807af04c5cebc33500ea5bc54a6feb9
Antonio Morrone <antonio@iovlabs.org>
2022-11-14 09:34:26 +0100

    build: include tpyes not automatically exported by typechain

--- index.ts
+++ index.ts
@@ -1,2 +1,6 @@
 export * from './typechain-types'; 
 export * from './scripts/deploy';
+
+// We need to explicitly export those types because they're not automatically exported by typechain
+export { EnvelopingTypes } from './typechain-types/contracts/RelayHub';
+export { TypedEvent } from './typechain-types/common';
