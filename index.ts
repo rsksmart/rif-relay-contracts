201517f1f642acc1d97ba50cfb872e8b4582b5a4
Francisco Tobar <francisco.tobar@iovlabs.org>
2022-11-17 12:52:04 -0600

    refactor(index): expose typing for relay-server

--- index.ts
+++ index.ts
@@ -2,5 +2,5 @@ export * from './typechain-types';
 export * from './scripts/deploy';
 
 // We need to explicitly export those types because they're not automatically exported by typechain
-export { EnvelopingTypes } from './typechain-types/contracts/RelayHub';
+export { EnvelopingTypes, StakeUnlockedEvent } from './typechain-types/contracts/RelayHub';
 export { TypedEvent } from './typechain-types/common';
