0b94c880db169cf16ff66d52582c870c8800aafa
Christos Otarola <christos@Christoss-MacBook-Pro.local>
2023-02-16 07:10:37 -0500

    feat: include token address on removal

--- contracts/Collector.sol
+++ contracts/Collector.sol
@@ -98,7 +98,8 @@ contract Collector is ICollector {
         return _tokens;
     }
 
-    function removeToken(uint256 tokenIndex) external onlyOwner {
+    function removeToken(address token, uint256 tokenIndex) external onlyOwner {
+        require(_tokens[tokenIndex] == token, 'Incorrect token');
         require(
             _tokens[tokenIndex].balanceOf(address(this)) == 0,
             "There is balance to share"
